ccbb21fc716642322805dac3bc2e06225354cd81
Sascha Graeff <sascha@blackicegames.de>
2015-05-30 01:23:29 +0200

    Integrated CharacterMotor into AIPlayer.

--- Assets/Scripts/AI/AIPlayer.cs
+++ Assets/Scripts/AI/AIPlayer.cs
@@ -3,19 +3,36 @@
 /// <summary>
 /// Die Komponente, die Peters Bewegungsscript steuert.
 /// </summary>
+[RequireComponent(typeof(CharacterMotor))]
 public class AIPlayer : MonoBehaviour
 {
     public static AIPlayer current { private set; get; }
+    private CharacterMotor motor;
     private AIWaypoint currentWaypoint;
 
 	void Awake()
     {
         current = this;
+        motor = GetComponent<CharacterMotor>();
 	}
 	
-	void Update()
+	void FixedUpdate()
     {
-	
+	    if(currentWaypoint)
+        {
+            var horizontalDistance = currentWaypoint.transform.position.x - transform.position.x;
+
+            var waypointReachDistance = 0.2f;
+
+            if(horizontalDistance < -waypointReachDistance)
+            {
+                motor.MoveLeft();
+            }
+            else if(horizontalDistance > waypointReachDistance)
+            {
+                motor.MoveRight();
+            }
+        }
 	}
 
     internal void SetWaypoint(AIWaypoint waypoint)
