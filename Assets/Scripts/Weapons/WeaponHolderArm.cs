9dda9b65bad72555e2b984538aeb35387525703f
Sascha Graeff <sascha@blackicegames.de>
2015-05-30 22:30:47 +0200

    WeaponHolderArm now rotates to somewhere.

--- Assets/Scripts/Weapons/WeaponHolderArm.cs
+++ Assets/Scripts/Weapons/WeaponHolderArm.cs
@@ -6,12 +6,14 @@ public class WeaponHolderArm : MonoBehaviour
     private SpriteRenderer r;
     private Sprite defaultSprite;
     private Transform armRoot;
+    private CharacterMotor motor;
 
 	void Awake()
     {
         r = GetComponent<SpriteRenderer>();
         defaultSprite = r.sprite;
         armRoot = transform.parent;
+        motor = transform.root.GetComponent<CharacterMotor>();
 	}
 	
 	public Sprite sprite
@@ -28,9 +30,13 @@ public class WeaponHolderArm : MonoBehaviour
     
     void LateUpdate()
     {
-        if(r.sprite != defaultSprite)
+        var weaponEquipped = r.sprite != defaultSprite;
+
+        if(weaponEquipped)
         {
             transform.localRotation = Quaternion.Euler(0, 0, -90);
+            var target = transform.position + Vector3.right; //motor.GetAimTarget() oder so
+            armRoot.localRotation = Quaternion.LookRotation(Vector3.forward, target - transform.position) * Quaternion.Euler(0, 0, 180);
         }
     }
 
