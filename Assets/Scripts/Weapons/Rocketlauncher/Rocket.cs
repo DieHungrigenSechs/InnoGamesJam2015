6372652bfec37fbb81029f1fd2a3355d54e2477d
Peter Schauss <unrealsoftware@gmx.de>
2015-05-31 00:11:00 +0200

    Rocketlauncher explosion effects

--- Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
+++ Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
@@ -2,8 +2,11 @@
 
 public class Rocket:Projectiles 
 {
+    public GameObject explosion;
 
-   
-
+    protected void OnCollisionEnter2D(Collision2D collision) {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        base.OnCollisionEnter2D(collision);
+    }
 }
 
