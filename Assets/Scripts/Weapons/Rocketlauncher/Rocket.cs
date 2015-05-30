b1366c00c8fbfe35425938d007901957fef1b7c5
ben <benedikt.bessen@gmx.de>
2015-05-30 23:59:42 +0200

    add basic klass for projectiles and remove health from player

--- Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
+++ Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 
-public class Rocket:MonoBehaviour {
+public class Rocket:Projectiles 
+{
 
-    protected void OnCollisionEnter2D(Collision2D collision) {
-        Destroy(gameObject);
-    }
+   
 
 }
 
