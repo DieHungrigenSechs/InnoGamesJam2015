d8cb226e5098f1461481d0bb0cb0344893f71183
ben <benedikt.bessen@gmx.de>
2015-05-30 23:22:36 +0200

    adding some basic assets for the menu and create basic graphic scenes

--- Assets/Scripts/Menu/Credits.cs
+++ Assets/Scripts/Menu/Credits.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Credits : BasicMenu 
 {
+	[SerializeField] string[] names;
+	[SerializeField] Text textField;
+	[SerializeField] float speed;
+	protected override void Start ()
+	{
+		base.Start ();
+		StartCoroutine(ShowNextUser(speed,0));
+	}
 
+	private IEnumerator ShowNextUser(float time,int id)
+	{
+		yield return new WaitForSeconds(time);
+		if(id < names.Length)
+		{
+			textField.text = names[id];
+			id++;
+			StartCoroutine(ShowNextUser(time,id));
+		}
+		else
+		{
+			GameObject.FindObjectOfType<BasicMenu>().LoadLevel(LevelEnum.MainMenu);
+		}
+	}
 }
