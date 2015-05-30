05baa1d283b24f77193be750cf8a3f5036f9ae99
Peter Schauss <unrealsoftware@gmx.de>
2015-05-30 03:02:58 +0200

    Character controller stuff

--- Assets/Scripts/CharacterController/CharacterMotor.cs
+++ Assets/Scripts/CharacterController/CharacterMotor.cs
@@ -1,33 +1,95 @@
 ﻿using UnityEngine;
 
-public class CharacterMotor : MonoBehaviour, ICharacterMotor
+public class CharacterMotor : MonoBehaviour
 {
-    private float currentSpeed = 0.1f;
+    [SerializeField]
+    private float maxSpeed = 15f;
 
+    [SerializeField]
+    private float accelerationSpeed = 3f;
+
+    [SerializeField]
+    private float decelerationSpeed = 5f;
+
+    [SerializeField]
+    private float jumpPower = 5;
+
+    private Rigidbody2D rigidbodyObject;
+
+    private float currentSpeed;
+
+    private float jumpPowerToApply;
+    
     private bool onGround = false;
 
-    private void Start()
+    private bool accelerated;
+
+    private void Awake()
     {
-        
+        rigidbodyObject = GetComponent<Rigidbody2D>();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         UpdateCharacterState();
     }
 
-    public void UpdateCharacterState() {
+    private void OnCollisionEnter() {
+        onGround = true;
     }
 
-    public void MoveLeft() {
-        transform.localPosition -= new Vector3(currentSpeed, 0, 0);
+    private void OnCollisionExit() {
+        onGround = false;
+    }
+
+    public void UpdateCharacterState()
+    {
+        // On ground?
+        //Transform transform = gameObject.transform;
+        //Vector3 position = transform.position;
+        //RaycastHit2D hit = Physics2D.Linecast(new Vector2(position.x, position.y) + new Vector2(0f, -(transform.localScale.y / 2f + 0.01f)), new Vector2(0, 0.1f));
+        //onGround = (hit.collider != null);
+        //Debug.Log(hit.collider);
+
+        // Movement X axis
+        rigidbodyObject.AddForce(new Vector2(currentSpeed, 0f));
+        if (accelerated) {
+            accelerated = false;
+        } else {
+            if (currentSpeed > 0f) {
+                currentSpeed = Mathf.Max(0f, currentSpeed - decelerationSpeed);
+            } else if (currentSpeed < 0f) {
+                currentSpeed = Mathf.Min(0f, currentSpeed + decelerationSpeed);
+            }
+        }
+
+        // Movement y axis
+        if (jumpPowerToApply > 0f) { //&& onGround) {
+            Debug.Log("jump");
+            rigidbodyObject.AddForce(new Vector2(0f, jumpPowerToApply), ForceMode2D.Impulse);
+        }
+        jumpPowerToApply = 0f;
+    }
+
+    public void MoveLeft()
+    {
+        accelerated = true;
+        ChangeSpeed(-accelerationSpeed);
     }
 
     public void MoveRight() {
-        transform.localPosition -= new Vector3(-currentSpeed, 0, 0);
+        accelerated = true;
+        ChangeSpeed(accelerationSpeed);
+    }
+
+    private void ChangeSpeed(float change)
+    {
+        currentSpeed = Mathf.Clamp(currentSpeed + change, -maxSpeed, maxSpeed);
     }
 
     public void Jump() {
-
+        if (rigidbodyObject.velocity.y <= 0f) {
+            jumpPowerToApply = jumpPower;
+        }
     }
 
 }
---
+++ Assets/Scripts/Physics/PhysicsSetup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+class PhysicsSetup : MonoBehaviour {
+
+    Vector2 Gravity = new Vector2(0, -15f);
+
+    public void Awake() {
+        Physics2D.gravity = Gravity;
+        Physics2D.raycastsStartInColliders = false;
+    }
+
+}
+
