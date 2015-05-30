341b69ae48bfd14e06fea8b18d49ed29d3f8ec54
ben <benedikt.bessen@gmx.de>
2015-05-30 10:41:55 +0200

    add tween framework for tween objects

---
+++ Assets/Scripts/BugPhysics/BugPhysics.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugPhysics : MonoBehaviour 
+{
+	protected SpriteRenderer renderer;
+	protected virtual void Awake()
+	{
+		renderer = 	GetComponent<SpriteRenderer>();
+	}
+
+	protected virtual void Start()
+	{
+		
+	}
+
+	protected virtual void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/BugPhysics/WallGlitch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallGlitch : BugPhysics
+{
+	private Collider collider;
+	protected override void Awake ()
+	{
+		base.Awake ();
+		collider = GetComponent<Collider>();
+	}
+
+	protected override void Start ()
+	{
+		base.Start ();
+		SetActive(false);
+	}
+
+	protected void SetActive(bool active)
+	{
+		collider.isTrigger = active;
+	}
+
+	protected override void Update ()
+	{
+		base.Update ();
+		Color color = renderer.color;
+		color.a = Flash(color.a,1f,1f);
+		renderer.color = color;
+	}
+
+	private float Flash(float value,float speed,float max)
+	{
+		if(speed != 0)
+		{
+			speed = Time.time * speed  * 2 * Mathf.PI;
+			speed = Mathf.Cos( speed ) * max;
+			value = speed;
+		}
+		return value;	
+	}
+}
