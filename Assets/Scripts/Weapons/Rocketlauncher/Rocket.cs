25affe4dde1ccc1e2a4da6204edeeaa74c61d58d
Peter Schauss <unrealsoftware@gmx.de>
2015-05-31 01:42:11 +0200

    Rocket and bomb improvement

--- Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
+++ Assets/Scripts/Weapons/Rocketlauncher/Rocket.cs
@@ -10,16 +10,6 @@ public class Rocket:Projectiles
         rocketRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    protected void Update() {
-        //transform.rotation = Quaternion.SetLookRotation(rocketRigidbody.velocity);
-        //transform.rotation.SetLookRotation(rocketRigidbody.velocity);
-        transform.localRotation = Quaternion.Euler(0, 0, 0);
-        var target = rocketRigidbody.velocity;
-        var direction = target - new Vector2(transform.position.x, transform.position.y);
-        direction.x = Mathf.Abs(direction.x);
-        transform.localRotation = Quaternion.LookRotation(Vector3.forward, direction) * Quaternion.Euler(0, 0, 180);
-    } 
-
     protected void OnCollisionEnter2D(Collision2D collision) {
         Instantiate(explosion, transform.position, Quaternion.identity);
         base.OnCollisionEnter2D(collision);
