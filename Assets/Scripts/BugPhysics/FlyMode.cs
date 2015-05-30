f313a38d3442d48c6c9095a1e87044916d393ba8
ben <benedikt.bessen@gmx.de>
2015-05-30 12:46:14 +0200

    add fly mode for have  fun

---
+++ Assets/Scripts/BugPhysics/FlyMode.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyMode : BugPhysics 
+{
+	private Rigidbody2D rigidbody;
+	private CharacterMotor characterMotor;
+	[SerializeField] float speed = 500f;
+	float tempGravity;
+
+	protected override void Awake ()
+	{
+		base.Awake ();
+		rigidbody = GetComponent<Rigidbody2D>(); 
+		characterMotor = GetComponent<CharacterMotor>();
+	}
+
+	protected override void Start ()
+	{
+		base.Start ();
+		
+		tempGravity = rigidbody.gravityScale;
+		SetGravity(0);
+	}
+
+	private void SetGravity(float value)
+	{
+		rigidbody.gravityScale = value;
+	}
+
+	protected void FixedUpdate ()
+	{
+		float speed = this.speed * Time.deltaTime;
+		Vector2 direction = Vector2.up * Input.GetAxis("Vertical");
+		rigidbody.AddForce(direction * speed);
+	}
+
+	protected override void Reset ()
+	{
+		base.Reset ();
+		SetGravity(tempGravity);
+	}
+}
