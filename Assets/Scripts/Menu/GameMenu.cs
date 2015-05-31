a14383795176b6198a15a3754c434004e5b9c970
ben <benedikt.bessen@gmx.de>
2015-05-31 07:48:26 +0200

    fixed some bugs

--- Assets/Scripts/Menu/GameMenu.cs
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -3,5 +3,9 @@ using System.Collections;
 
 public class GameMenu : BasicMenu 
 {
-
+	public override void LoadLevel (LevelEnum level)
+	{
+		base.LoadLevel (level);
+		Cursor.visible = true;
+	}
 }
