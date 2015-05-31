253c8dee22701fe324e3ca274cfff31a67631b36
ben <benedikt.bessen@gmx.de>
2015-05-31 02:02:16 +0200

    add hud

---
+++ Assets/Scripts/HUD/BasicHud.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicHud : MonoBehaviour {
+
+	protected virtual void Awake () 
+	{
+		
+	}
+	// Use this for initialization
+	protected virtual void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+	
+	}
+}
