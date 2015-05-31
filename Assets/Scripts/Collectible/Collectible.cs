170434506ad2db939d5a8eba621316b785ed12f8
Peter Schauss <unrealsoftware@gmx.de>
2015-05-31 04:01:02 +0200

    Collectable Glitches

---
+++ Assets/Scripts/Collectible/Collectible.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public abstract class Collectible : MonoBehaviour {
+
+    private float deSpawnTime = 20f;
+
+    private float speed = 0.5f;
+
+    private Rigidbody2D rigidbodyComponent;
+
+    private float oldx = 0f;
+
+    private float lastChange = 0f;
+
+    protected void Start() {
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, deSpawnTime);
+    }
+
+    protected void FixedUpdate() {
+        rigidbodyComponent.AddForce(new Vector2(speed, 0f));
+        if (transform.position.x == oldx && Mathf.Abs(Time.timeSinceLevelLoad - lastChange) > 0.5f) {
+            speed = -speed;
+            lastChange = Time.timeSinceLevelLoad;
+        }
+        oldx = transform.position.x;
+    }
+
+    protected void OnCollisionEnter2D(Collision2D other) {
+        if (other.transform.GetComponent<CharacterInput>()) {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Collectible/HealthCollectible.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HealthCollectible : Collectible {
+
+    protected void OnCollisionEnter2D(Collision2D other) {
+        if (other.transform.GetComponent<CharacterInput>()) {
+		    PlayerHealth health = other.gameObject.GetComponent<PlayerHealth>();
+		    if(health)
+		    {
+                Debug.Log("HEALTH");
+			    health.SetEnergy(50);
+                Destroy(gameObject);
+		    }
+        }
+    }
+
+}
